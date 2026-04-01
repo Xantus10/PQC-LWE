cb91d9b375f16b8787b849853dcdf19a1ef833bc
xantus10 <jaroslav.zaba@post.cz>
2026-04-01 18:24:44 +0200

    LWE encapsulation

--- lwe.rb
+++ lwe.rb
@@ -1,36 +1,70 @@
 # frozen_string_literal: true
 
-require_relative 'math_helper'
-require_relative 'random_helper'
-require_relative 'vector'
+require_relative 'helper/math_helper'
+require_relative 'helper/random_helper'
+require_relative 'helper/vector'
+require_relative 'helper/coder'
 
 # LWE algorithm for public key cryptography
 class LWE
   # Default constructor
   # @param dimensions_n [Integer] The dimensions of the vectors
   # @param modulo_q [Integer] The domain of integers
-  def initialize(dimensions_n, modulo_q)
+  # @param public_matrix [Array<Vector>] The public matrix (For encapsulation)
+  # @param public_vector [Vector] The public vector (For encapsulation)
+  def initialize(dimensions_n, modulo_q, public_matrix = nil, public_vector = nil)
     @dimensions = dimensions_n
     @modulo = modulo_q
 
-    @secret = RandomHelper.random_vector(dimensions_n)
+    @coder = Coder.new(modulo_q)
 
-    @public_matrix = dimensions_n.times.map { RandomHelper.random_vector(dimensions_n) }
+    @secret = RandomHelper.random_small_vector(dimensions_n)
 
-    @public_vector = compute_public_vector
+    @public_matrix = public_matrix.nil? ? RandomHelper.random_matrix(dimensions_n) : public_matrix
+
+    @public_vector = public_vector.nil? ? compute_public_vector(@secret) : public_vector
   end
 
-  def compute_public_vector
-    MathHelper.matrix_vector_multiplication(@public_matrix, @secret) + RandomHelper.random_vector
+  private
+
+  # Compute the public vector
+  # @param [Vector] secret The secret
+  # @return [Vector] The public vector
+  def compute_public_vector(secret)
+    (MathHelper.matrix_vector_multiplication(@public_matrix, secret) +
+      RandomHelper.random_small_vector(@dimensions)) % @modulo
   end
+
+  public
 
   # Get the public key
-  # @return [Hash{:matrix => Array<Vector>, :vector => Vector
-  # }]
+  # @return [Hash]
+  # @option return [Array<Vector>] :matrix The public matrix
+  # @option return [Vector] :vector The public vector
   def public_key
     {
       matrix: @public_matrix,
       vector: @public_vector
     }
+  end
+
+  # Encapsulate the message
+  #
+  # The primitive LWE supports encryption only bit by bit
+  # @param message [String] The message
+  # @return [Array<Hash>]
+  # @option return [Vector] :key
+  # @option return [Integer] :ciphertext
+  def encapsulate(message)
+    @coder.encode(message).map do |bit|
+      ephemeral = RandomHelper.random_small_vector(@dimensions)
+      key = compute_public_vector(ephemeral)
+      ciphertext = (ephemeral.dot_product(@public_vector) +
+        RandomHelper.random_small_int + bit) % @modulo
+      {
+        key: key,
+        ciphertext: ciphertext
+      }
+    end
   end
 end
